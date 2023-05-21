2dfd6444fc898b3d6ab09bf770ae9e42dcb2a808
Linux-1 <Linux-1@DESKTOP-T9VO9J2>
2023-05-20 20:58:18 -0700

    Agregar archivos de proyecto.

---
+++ Ejercicios.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pruebas
+{
+    internal class Ejercicios
+    {
+        // Peso KGS
+        public Ejercicios(string nombreEjercicio, int series, int repeticiones, Peso peso, string maquinaria, string grupoMuscular) 
+        {
+
+            if (nombreEjercicio == null )
+                nombreEjercicio = "Ejercicio";
+            Nombre = nombreEjercicio;
+
+            Series = series;
+
+            Repeticiones = repeticiones;
+
+            if (peso.Unidad == "kg")
+            {
+                PesoKG = peso.Cantidad;
+                PesoLBS = 0;
+            }
+            else
+            {
+                PesoLBS = peso.Cantidad;
+                PesoKG = 0;
+            }
+
+            if (maquinaria == null)
+                maquinaria = "Sin especificar";
+            Maquinaria = maquinaria;
+
+            if (grupoMuscular == null)
+                grupoMuscular = "Sin especificar";
+            GrupoMuscular = grupoMuscular;
+
+        }
+
+        public string Nombre
+        {
+            set { value = _nombre; }
+            get { return _nombre; }
+        }
+
+        public int Series
+        {
+            set { value = _series; }
+            get { return _series; }
+        }
+
+        public int Repeticiones
+        {
+            set { value = _repeticiones; }
+            get { return _repeticiones; }
+        }
+
+        public decimal PesoKG
+        {
+            set { value = _pesoKG; }
+            get { return _pesoKG; }
+        }
+
+        public decimal PesoLBS
+        {
+            set { value = _pesoLBS; }
+            get { return _pesoLBS; }
+        }
+
+        public string Maquinaria
+        {
+            set { value = _maquinaria; }
+            get { return _maquinaria; }
+        }
+
+        public string GrupoMuscular
+        {
+            set { value = _grupoMuscular; }
+            get { return _grupoMuscular; }
+        }
+
+        public Peso CambiarUnidadesPeso(Peso peso, decimal cantidad)
+        {
+            if (peso.Unidad == "kg")
+                return peso.ConvertirUnidad(cantidad);
+            else
+                return peso.ConvertirUnidad(cantidad);
+        }
+
+        private string _nombre;
+        private int _series;
+        private int _repeticiones;
+        private decimal _pesoKG;
+        private decimal _pesoLBS;
+        private string _maquinaria;
+        private string _grupoMuscular;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿// C# program to illustrate how
+// to read a file in C#
+using System;
+using System.IO;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        //Ignora las clases 
+
+        // Esto es el texto que se va a incluir en el archivo txt que crearas
+        string text = "This is some cola.";
+        // Esto es el path donde se va a crear el archivo y donde lo vas a leer
+        // el path es el camino que sigue la compu para encontrar el archivo
+        // El que tengo es uno absoluto, es decir, le digo que lo busque en ese lugar
+        // Especificando desde que disco encontrarlo, por eso solo funciona en mi pc
+        // Lo que quiero que hagas es que lo hagas relativo, es decir que funcione
+        // En cualquier compu
+
+        // j.txt es el nombre del archivo
+        // \archivos es su carpeta
+        // y asi sucesivamente
+        string path = @"C:\Users\Linux-1\source\repos\Pruebas\archivos\j.txt";
+
+        //Esto de aqui crea un archivo txt en el path que le asignes
+        // No necesitas entenderlo, no le muevas
+        using (FileStream oFS = File.Create(path)) 
+        {
+            Byte[] texto = new UTF8Encoding(true).GetBytes(text);
+            oFS.Write(texto, 0, texto.Length);
+        }
+
+        // Esto lee el texto que esta adentro del archivo
+        Console.WriteLine(File.ReadAllText(path));
+
+    }
+}
